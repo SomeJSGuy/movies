669a643a9942376229d5cfe835c74427c423ea43
Esteban Dosztal <edosztal@gmail.com>
2016-07-21 19:31:43 -0300

    Error del servidor

--- controllers/Movies.js
+++ controllers/Movies.js
@@ -2,25 +2,56 @@
 "use strict";
 
 const fdebug = require("../lib/fdebug");
-const debug = fdebug("movies:controllers:movies");
+const request = require("request");
 
 
 function Movies(main) {
-    debug("init.");
+    fdebug("init.");
     return {
 
         'search': (req, res, next)=>{
-            debug(".search called");
+            fdebug(".search called");
 
             var title  = req.swagger.params.title ? req.swagger.params.title.value : null;
             var year  = req.swagger.params.year ? req.swagger.params.year.value : null;
             var id  = req.swagger.params.id ? req.swagger.params.id.value : null;
 
             main.libs.Movies.search({title: title, year: year, id: id})
-            .then((movies)=>{
+            .then((movies) => {
+                    if (movies.length === 0) {
+                        Movies.searchOmbd(req, res, next).then((movies2) => {
+                            res.json(movies2);
+                        });
+                    }
+
                     res.json(movies);
                 })
             .catch(next);
+        },
+        'searchOmbd': (req, res, next) => {
+            fdebug(".searchOmdb called");
+
+            var title = req.swagger.params.title ?
+                            req.swagger.params.title.value :
+                            null,
+                url = 'http://www.omdbapi.com/?';
+
+            if (title != null) {
+                url = url + 't=' + title;
+            }
+
+            request(url, (err, res, body) => {
+                if (err) {
+                    // manejar error y salir
+                    console.error('Error: ', error);
+                    throw error;
+                }
+
+                if (res.statusCode == 200) {
+                    console.log('body: ', body);
+                    res(body);
+                }
+            });
         }
 
     };//end return
